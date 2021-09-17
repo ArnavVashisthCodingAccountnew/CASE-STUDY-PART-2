937493a2073beca07b973e16c394e5e822b3c7fb
Arnav Vashisth <arnavvst2020@gmail.com>
2021-09-17 12:15:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function disc1(){
+    alert("Microsoft Azure:  a close up of a device        Moblinet :      IS MORE ACCURATE");
+}
+function disc2(){
+    alert("Microsoft Azure:  a close up of a remote control        Moblinet :      IS MORE ACCURATE");
+}
+function disc3(){
+    alert("Microsoft Azure:  a small room        Moblinet :      IS MORE ACCURATE");
+}
+function disc4(){
+    alert("Microsoft Azure:   undefined       Moblinet :      IS MORE ACCURATE");
+}
+function disc5(){
+    alert("Microsoft Azure:   a close up of a game       Moblinet :      IS MORE ACCURATE");
+}
+function disc6(){
+    alert("Microsoft Azure:   shower curtain       Moblinet :      IS MORE ACCURATE");
+}
+function disc8(){
+    alert("Microsoft Azure:   a bedroom with a bed and chair       Moblinet :      IS MORE ACCURATE");
+}
+function disc7(){
+    alert("Microsoft Azure:   a cup of coffee       Moblinet :      IS MORE ACCURATE");
+}
+function disc9(){
+    alert("Microsoft Azure:   undefined       Moblinet :      IS MORE ACCURATE");
+}
